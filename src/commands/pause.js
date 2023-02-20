b2dc8dd9eb96125024d3561a5d684d4bc847ec68
trieuden <119589795+trieuden@users.noreply.github.com>
2023-02-20 16:40:16 +0700

    edit file

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -23,6 +23,6 @@ module.exports = {
             return interaction.reply({ content: `❌ | There is no music currently playing!.`, allowedMentions: { repliedUser: false } });
 
         const success = queue.setPaused(true);
-        return success ? interaction.reply("⏸️ | Music paused.") : interaction.reply({ content: `❌ | Something went wrong.`, allowedMentions: { repliedUser: false } });
+        return success ? interaction.reply("⏸️ | Music paused.") : interaction.reply({ content: `❌ | Sai rồi cưng ơi.`, allowedMentions: { repliedUser: false } });
     },
 };
